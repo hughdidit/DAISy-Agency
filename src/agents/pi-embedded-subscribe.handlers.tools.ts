844ec3d8947f927100b0978b5215a8d551035223
Peter Steinberger <steipete@gmail.com>
2026-02-01 14:52:11 -0800

    fix: wire before_tool_call hook into tool execution (#6570) (thanks @ryancnelson) (#6660)
    (cherry picked from commit 8eb11bd3044a21eb6ce76e8a451ae94ec4ab0549)
    
    # Conflicts:
    #	CHANGELOG.md
    #	README.md
    #	src/agents/pi-embedded-subscribe.handlers.tools.ts

--- src/agents/pi-embedded-subscribe.handlers.tools.ts
+++ src/agents/pi-embedded-subscribe.handlers.tools.ts
@@ -1,5 +1,9 @@
 import type { AgentEvent } from "@mariozechner/pi-agent-core";
+<<<<<<< HEAD
 
+=======
+import type { EmbeddedPiSubscribeContext } from "./pi-embedded-subscribe.handlers.types.js";
+>>>>>>> 8eb11bd30 (fix: wire before_tool_call hook into tool execution (#6570) (thanks @ryancnelson) (#6660))
 import { emitAgentEvent } from "../infra/agent-events.js";
 import { normalizeTextForComparison } from "./pi-embedded-helpers.js";
 import { isMessagingTool, isMessagingToolSendAction } from "./pi-embedded-messaging.js";
