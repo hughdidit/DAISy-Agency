32ccf5313e39b8636f4a572b6841a09ab5837f54
Peter Steinberger <steipete@gmail.com>
2026-02-18 02:10:37 +0100

    Revert "Add mesh orchestration gateway methods with DAG execution and retry"
    This reverts commit 83990ed54269ffca5d25c5667c333ddc42f58780.
    
    (cherry picked from commit 972d1b74d0d9d3d58615a66612800bef4ea36bc1)
    
    # Conflicts:
    #	src/gateway/server-methods.ts

--- src/gateway/protocol/schema/protocol-schemas.ts
+++ src/gateway/protocol/schema/protocol-schemas.ts
@@ -103,13 +103,6 @@ import {
   LogsTailResultSchema,
 } from "./logs-chat.js";
 import {
-  MeshPlanParamsSchema,
-  MeshRetryParamsSchema,
-  MeshRunParamsSchema,
-  MeshStatusParamsSchema,
-  MeshWorkflowPlanSchema,
-} from "./mesh.js";
-import {
   NodeDescribeParamsSchema,
   NodeEventParamsSchema,
   NodeInvokeParamsSchema,
@@ -258,11 +251,6 @@ export const ProtocolSchemas: Record<string, TSchema> = {
   ChatAbortParams: ChatAbortParamsSchema,
   ChatInjectParams: ChatInjectParamsSchema,
   ChatEvent: ChatEventSchema,
-  MeshPlanParams: MeshPlanParamsSchema,
-  MeshWorkflowPlan: MeshWorkflowPlanSchema,
-  MeshRunParams: MeshRunParamsSchema,
-  MeshStatusParams: MeshStatusParamsSchema,
-  MeshRetryParams: MeshRetryParamsSchema,
   UpdateRunParams: UpdateRunParamsSchema,
   TickEvent: TickEventSchema,
   ShutdownEvent: ShutdownEventSchema,
