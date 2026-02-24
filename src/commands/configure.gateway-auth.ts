d18b5a89ed73422932c38ce4e0444eadcd51033b
Gustavo Madeira Santana <gumadeiras@gmail.com>
2026-02-23 21:30:59 -0500

    chore(provider): remove unused pruning functions
    (cherry picked from commit 207ec7cfae47b7da291adb6d0c673e8019af021f)

--- src/commands/configure.gateway-auth.ts
+++ src/commands/configure.gateway-auth.ts
@@ -8,7 +8,6 @@ import {
   applyModelAllowlist,
   applyModelFallbacksFromSelection,
   applyPrimaryModel,
-  pruneKilocodeProviderModelsToAllowlist,
   promptDefaultModel,
   promptModelAllowlist,
 } from "./model-picker.js";
@@ -130,7 +129,6 @@ export async function promptAuthConfig(
     });
     if (allowlistSelection.models) {
       next = applyModelAllowlist(next, allowlistSelection.models);
-      next = pruneKilocodeProviderModelsToAllowlist(next, allowlistSelection.models);
       next = applyModelFallbacksFromSelection(next, allowlistSelection.models);
     }
   }
