45e97c9652f69a3fb5bd63c616469ca4253ba043
Ayaan Zaidi <zaidi@uplause.io>
2026-02-25 09:21:18 +0530

    build(android): migrate to AGP 9 new DSL kotlin setup
    (cherry picked from commit 1edd9f8bf54875a947e55519721f772066f8fcfa)
    
    # Conflicts:
    #	apps/android/build.gradle.kts
    #	apps/android/gradle.properties

--- apps/android/build.gradle.kts
+++ apps/android/build.gradle.kts
@@ -1,6 +1,10 @@
 plugins {
+<<<<<<< HEAD
   id("com.android.application") version "8.13.2" apply false
   id("org.jetbrains.kotlin.android") version "2.2.21" apply false
+=======
+  id("com.android.application") version "9.0.1" apply false
+>>>>>>> 1edd9f8bf (build(android): migrate to AGP 9 new DSL kotlin setup)
   id("org.jetbrains.kotlin.plugin.compose") version "2.2.21" apply false
   id("org.jetbrains.kotlin.plugin.serialization") version "2.2.21" apply false
 }
