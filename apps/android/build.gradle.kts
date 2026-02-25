980b3fffb212a0a4876c497015d8a28f4507590d
Ayaan Zaidi <zaidi@uplause.io>
2026-02-25 21:34:15 +0530

    perf(android): tighten startup path and add perf tooling
    (cherry picked from commit b49c2cbdd9c3108ae665e48d23be9dc31ff39f56)
    
    # Conflicts:
    #	apps/android/app/src/main/java/bot/molt/android/SecurePrefs.kt
    #	apps/android/build.gradle.kts

--- apps/android/build.gradle.kts
+++ apps/android/build.gradle.kts
@@ -4,7 +4,11 @@ plugins {
   id("org.jetbrains.kotlin.android") version "2.2.21" apply false
 =======
   id("com.android.application") version "9.0.1" apply false
+<<<<<<< HEAD
 >>>>>>> 1edd9f8bf (build(android): migrate to AGP 9 new DSL kotlin setup)
+=======
+  id("com.android.test") version "9.0.1" apply false
+>>>>>>> b49c2cbdd (perf(android): tighten startup path and add perf tooling)
   id("org.jetbrains.kotlin.plugin.compose") version "2.2.21" apply false
   id("org.jetbrains.kotlin.plugin.serialization") version "2.2.21" apply false
 }
--- apps/android/settings.gradle.kts
+++ apps/android/settings.gradle.kts
@@ -16,3 +16,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "MoltbotNodeAndroid"
 include(":app")
+include(":benchmark")
