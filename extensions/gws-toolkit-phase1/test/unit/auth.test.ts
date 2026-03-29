ed0f4e2e78a9fb0778bd507fd8d12f3654e9dce2
hughdidit <hugh.chapman@hughdidit.com>
2026-03-29 10:32:01 -0700

    test(gws-toolkit): normalize auth fixture path assertion

--- extensions/gws-toolkit-phase1/test/unit/auth.test.ts
+++ extensions/gws-toolkit-phase1/test/unit/auth.test.ts
@@ -57,7 +57,7 @@ describe("auth resolution", () => {
     );
     expect(ok.mode).toBe("credentials_file");
     expect(ok.args).toEqual([]);
-    expect(ok.env.GOOGLE_WORKSPACE_CLI_CREDENTIALS_FILE).toBe(allowedFile);
+    expect(ok.env.GOOGLE_WORKSPACE_CLI_CREDENTIALS_FILE).toBe(await fs.realpath(allowedFile));
 
     expect(() =>
       resolveAuth(
